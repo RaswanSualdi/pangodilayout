07586c5a01ad6cc11ee5996622093f0e290fb272
Raswan <raswan@Macbooks-MacBook-Air.local>
2023-05-18 11:56:53 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+window.addEventListener("scroll", function() {
+  var akadDetails = document.querySelector(".section1 .quote");
+  var position = akadDetails.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight) {
+    akadDetails.classList.add("show");
+  }
+});
+
+
+window.addEventListener("scroll", function() {
+  var akadDetails = document.querySelector(".section4 .akad-details");
+  var position = akadDetails.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight) {
+    akadDetails.classList.add("show");
+  }
+});
+
+
+window.addEventListener("scroll", function() {
+  var akadDetails = document.querySelector(".section4 .resepsi-details");
+  var position = akadDetails.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight) {
+    akadDetails.classList.add("show");
+  }
+});
+
+
+window.addEventListener("scroll", function() {
+  var akadDetails = document.querySelector(".section5 .decoration-story .circle1");
+  var position = akadDetails.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight) {
+    akadDetails.classList.add("show");
+  }
+});
+
+window.addEventListener("scroll", function() {
+  var akadDetails = document.querySelector(".section5 .decoration-story .circle2");
+  var position = akadDetails.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight) {
+    akadDetails.classList.add("show");
+  }
+});
+
+window.addEventListener("scroll", function() {
+  var akadDetails = document.querySelector(".section5 .decoration-story .circle3");
+  var position = akadDetails.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight) {
+    akadDetails.classList.add("show");
+  }
+});
+
+window.addEventListener("scroll", function() {
+  var akadDetails = document.querySelector(".section5 .decoration-story .line1");
+  var position = akadDetails.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight) {
+    akadDetails.classList.add("show");
+  }
+});
+
+window.addEventListener("scroll", function() {
+  var akadDetails = document.querySelector(".section5 .decoration-story .line2");
+  var position = akadDetails.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight) {
+    akadDetails.classList.add("show");
+  }
+});
