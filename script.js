0697233c27129e2c2baf3a60f13588c131498dc5
Raswan <raswan@Macbooks-MacBook-Air.local>
2023-05-19 17:43:30 +0800

    show image

--- script.js
+++ script.js
@@ -80,3 +80,21 @@ window.addEventListener("scroll", function() {
     akadDetails.classList.add("show");
   }
 });
+
+
+function showImage(element) {
+  var imageURL = element.getAttribute("src");
+  var modal = document.createElement("div");
+  modal.classList.add("modal");
+  modal.innerHTML = '<span class="close" onclick="closeModal()">&times;</span><img src="' + imageURL + '" class="modal-content">';
+  document.body.appendChild(modal);
+  document.body.style.overflow = "hidden";
+}
+
+function closeModal() {
+  var modal = document.querySelector(".modal");
+  if (modal) {
+    modal.parentNode.removeChild(modal);
+    document.body.style.overflow = "auto";
+  }
+}
